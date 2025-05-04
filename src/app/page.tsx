d25ee9e196a36fabe54c9fdb191c3aa8fc52082e
birtukan <birtukandegu2127@gmail.com>
2025-05-04 12:13:24 +0300

    implement theme toggle with support for light and dark modes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import Header from "@/components/Header";
-import Hero from "@/components/LandingPage/Hero";
+import Hero from "@/components/landingPage/Hero";
 import {
   Linkedin,
   Sparkles,
@@ -16,8 +16,8 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import AnimatedCounter from "../components/LandingPage/AnimatedCounter";
-import StarSpin from "@/components/NotFound/StarSpin";
+import AnimatedCounter from "../components/landingPage/AnimatedCounter";
+import StarSpin from "@/components/notFound/StarSpin";
 import { motion, easeInOut } from "framer-motion";
 // import DemoPreview from "./components/demoPreview";
 
@@ -36,16 +36,15 @@ const Home = () => {
     },
   };
 
-
   return (
     <div className="flex justify-center items-center overflow-x-hidden">
-      <div className="border-l border-r max-w-4xl w-full mx-auto border-dashed h-full flex justify-center flex-col border-gray-300">
+      <div className="border-l border-r max-w-4xl w-full mx-auto border-dashed h-full flex justify-center flex-col border-gray-300 dark:border-gray-800">
         <div className="flex flex-col justify-center items-center mt-10">
           <Header text="Start shelving with" />
           <Hero />
         </div>
         <div className="justify-center items-center h-full flex flex-col my-10">
-          <div className="border-t border-dashed border-gray-300 w-full mx-auto py-10 flex justify-center items-center relative">
+          <div className="border-t border-dashed border-gray-300 dark:border-gray-800 w-full mx-auto py-10 flex justify-center items-center relative">
             <Plus className="text-orange-500 group-hover:text-orange-500 w-7 h-7 absolute top-0 -translate-x-1/2 -translate-y-1/2 left-0" />
             <Image
               src="/images/shelfee.svg"
@@ -56,14 +55,14 @@ const Home = () => {
             />
           </div>
           <div className="relative group">
-            <h1 className="font-semibold text-3xl md:text-7xl text-center w-full border-t border-b py-6 border-dashed text-balance px-4 text-black border-gray-300">
+            <h1 className="font-semibold text-3xl md:text-7xl text-center w-full border-t border-b py-6 border-dashed text-balance px-4 text-black dark:text-brand-cream border-gray-300 dark:border-gray-800">
               <span className="">Let&apos;s build a lasting </span>
               <span className="text-transparent bg-clip-text bg-gradient-to-br from-brand-green to-brand-light-green">
                 reading habit.
               </span>
             </h1>
           </div>
-          <div className="pt-10 pb-11 flex gap-3 items-center mx-auto border-gray-300 border-b w-full justify-center border-dashed relative">
+          <div className="pt-10 pb-11 flex gap-3 items-center mx-auto border-gray-300 dark:border-gray-800 border-b w-full justify-center border-dashed relative">
             <Plus className="text-orange-500 group-hover:text-orange-500 w-7 h-7 absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2" />
             <Link
               href="/my-shelf"
@@ -93,9 +92,9 @@ const Home = () => {
               Github
             </Link>
           </div>
-          <div className=" group border-b  border-dashed text-balance text-center w-full font-semibold border-gray-300 ">
+          <div className=" group border-b  border-dashed text-balance text-center w-full font-semibold border-gray-300 dark:border-gray-800 ">
             <div className="grid grid-cols-2 px-4 relative py-6 text-3xl md:text-7xl max-w-lg mx-auto">
-              <div className="flex flex-col border-r border-dashed border-gray-300">
+              <div className="flex flex-col border-r border-dashed border-gray-300 dark:border-gray-800">
                 <div className="flex justify-center items-center gap-2 text-brand-green">
                   <Users className="w-4 h-4" />
                   <span className="text-xs">Trusted by</span>
@@ -119,61 +118,61 @@ const Home = () => {
           </div>
         </div>
         <div className="mb-10">{/* <DemoPreview /> */}</div>
-        <p className="font-medium text-xl md:text-3xl border-t border-gray-300 border-b border-dashed md:py-6 py-3 px-7 text-neutral-700 text-center">
+        <p className="font-medium text-xl md:text-3xl border-t border-gray-300 dark:border-gray-800 border-b border-dashed md:py-6 py-3 px-7 text-neutral-700 dark:text-brand-cream text-center">
           Here&apos;s why you should use Shelfee
         </p>
         <div className="grid sm:grid-cols-2 relative">
           <Plus className="text-orange-500 group-hover:text-orange-500 w-7 h-7 absolute top-0 -translate-x-1/2 -translate-y-1/2 left-0" />
           <Plus className="text-orange-500 group-hover:text-orange-500 w-7 h-7 absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2" />
-          <div className="border-r border-b border-dashed flex justify-center flex-col items-center p-7 border-gray-300">
-            <p className="border flex justify-center items-center w-11 h-11 border-dashed rounded-lg text-2xl text-brand-green mb-5 border-gray-300 bg-gradient-to-br from-brand-light-green to-brand-cream">
+          <div className="border-r border-b border-dashed flex justify-center flex-col items-center p-7 border-gray-300 dark:border-gray-800">
+            <p className="border flex justify-center items-center w-11 h-11 border-dashed rounded-lg text-2xl text-brand-green mb-5 border-gray-300 dark:border-gray-800 bg-gradient-to-br from-brand-light-green to-brand-cream">
               <Blocks />
             </p>
             <p className="font-bold text-xl text-brand-green">
               Move across shelves
             </p>
-            <p className="text-neutral-500 mt-1 text-center">
+            <p className="text-neutral-500 dark:text-brand-cream mt-1 text-center">
               Organize your books into categories as you start, pause, or finish
               them.
             </p>
           </div>
-          <div className="border-b border-dashed p-7 border-gray-300 flex justify-center flex-col items-center">
-            <p className="border flex justify-center items-center w-11 h-11 border-dashed rounded-lg text-2xl text-brand-green mb-5 border-gray-300 bg-gradient-to-br from-brand-light-green to-brand-cream">
+          <div className="border-b border-dashed p-7 border-gray-300 dark:border-gray-800 flex justify-center flex-col items-center">
+            <p className="border flex justify-center items-center w-11 h-11 border-dashed rounded-lg text-2xl text-brand-green mb-5 border-gray-300 dark:border-gray-800 bg-gradient-to-br from-brand-light-green to-brand-cream">
               <NotebookPen />
             </p>
             <p className="font-bold text-xl text-brand-green">
               Notes and highlights
             </p>
-            <p className="text-neutral-500 mt-1 text-center">
+            <p className="text-neutral-500 dark:text-brand-cream mt-1 text-center">
               Jot down reflections, favorite quotes, or thoughts directly in
               your Shelfee entries.
             </p>
           </div>
-          <div className="border-r border-b border-dashed flex justify-center flex-col items-center p-7 border-gray-300">
-            <p className="border flex justify-center items-center w-11 h-11 border-dashed rounded-lg text-2xl text-brand-green mb-5 border-gray-300 bg-gradient-to-br from-brand-light-green to-brand-cream">
+          <div className="border-r border-b border-dashed flex justify-center flex-col items-center p-7 border-gray-300 dark:border-gray-800">
+            <p className="border flex justify-center items-center w-11 h-11 border-dashed rounded-lg text-2xl text-brand-green mb-5 border-gray-300 dark:border-gray-800 bg-gradient-to-br from-brand-light-green to-brand-cream">
               <Sparkles />
             </p>
             <p className="font-bold text-xl text-brand-green">
               Minimal, distruction-free design
             </p>
-            <p className="text-neutral-500 mt-1 text-center">
+            <p className="text-neutral-500 dark:text-brand-cream mt-1 text-center">
               Shelfee is built with simplicity in mind. No clutter. Just your
               books.
             </p>
           </div>
-          <div className="border-b border-gray-300 border-dashed flex justify-center flex-col items-center p-7">
-            <p className="border flex justify-center items-center w-11 h-11 border-dashed rounded-lg text-2xl text-brand-green mb-5 border-gray-300 bg-gradient-to-br from-brand-light-green to-brand-cream">
+          <div className="border-b border-gray-300 dark:border-gray-800 border-dashed flex justify-center flex-col items-center p-7">
+            <p className="border flex justify-center items-center w-11 h-11 border-dashed rounded-lg text-2xl text-brand-green mb-5 border-gray-300 dark:border-gray-800 bg-gradient-to-br from-brand-light-green to-brand-cream">
               <FolderSearch />
             </p>
             <p className="font-bold text-xl text-brand-green">
               Search & Filter
             </p>
-            <p className="text-neutral-500 mt-1 text-center">
+            <p className="text-neutral-500 dark:text-brand-cream mt-1 text-center">
               Easily find books in your shelf — by title, author, or status.
             </p>
           </div>
         </div>
-        <div className="px-5 py-11 flex justify-center items-center border-b border-dashed border-gray-300">
+        <div className="px-5 py-11 flex justify-center items-center border-b border-dashed border-gray-300 dark:border-gray-800">
           <div className="flex justify-center rounded-2xl max-w-3xl border-dashed items-center px-4 py-10 w-full flex-col gap-4  bg-gradient-to-br from-brand-light-green to-brand-cream">
             <p className="md:max-w-2xl text-center font-semibold text-xl text-black p-4 md:text-3xl text-balance">
               Track what you&apos;re reading, what you&apos;ve finished, and
@@ -187,7 +186,7 @@ const Home = () => {
             </Link>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center border-b border-dashed mb-20 border-gray-300 py-10 px-4 gap-4">
+        <div className="flex flex-col justify-center items-center border-b border-dashed mb-20 border-gray-300 dark:border-gray-800 py-10 px-4 gap-4">
           <motion.div
             variants={appear}
             initial="initial"
@@ -197,8 +196,11 @@ const Home = () => {
             <StarSpin classNameSize="w-[20vw] md:w-[15vw]" />
           </motion.div>
           <div className="flex gap-4">
-            <div className="border flex justify-center items-center w-10 h-10 border-dashed rounded-lg text-2xl text-brand-green border-gray-300">
-              <Link target="_blank" href="https://github.com/BirtukanDegu/Shelfee">
+            <div className="border flex justify-center items-center w-10 h-10 border-dashed rounded-lg text-2xl text-brand-green border-gray-300 dark:border-gray-800">
+              <Link
+                target="_blank"
+                href="https://github.com/BirtukanDegu/Shelfee"
+              >
                 <svg
                   width="15"
                   height="15"
@@ -216,18 +218,20 @@ const Home = () => {
                 </svg>
               </Link>
             </div>
-            <div className="border flex justify-center items-center w-10 h-10 border-dashed rounded-lg text-2xl text-brand-green border-gray-300">
-              <Link target="_blank" href="https://www.linkedin.com/in/birtukan-degu-mulusew/">
+            <div className="border flex justify-center items-center w-10 h-10 border-dashed rounded-lg text-2xl text-brand-green border-gray-300 dark:border-gray-800">
+              <Link
+                target="_blank"
+                href="https://www.linkedin.com/in/birtukan-degu-mulusew/"
+              >
                 <Linkedin />
               </Link>
             </div>
-            <div className="border flex justify-center items-center w-10 h-10 border-dashed rounded-lg text-2xl text-brand-green border-gray-300">
+            <div className="border flex justify-center items-center w-10 h-10 border-dashed rounded-lg text-2xl text-brand-green border-gray-300 dark:border-gray-800">
               <a target="_blank" href="">
                 <Twitter />
               </a>
             </div>
           </div>
-
         </div>
       </div>
     </div>
