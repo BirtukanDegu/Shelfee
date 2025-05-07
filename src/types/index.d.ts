b42da6241b2c752c600363de6fdcb90eeafd498e
birtukan <birtukandegu2127@gmail.com>
2025-05-07 23:31:05 +0300

    Define TypeScript interfaces for book-related attributes

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,14 +1,63 @@
-declare interface AuthDataType {
+interface AuthDataType {
   username?: string;
   email: string;
   password: string;
   passwordConfirmation?: string;
 }
 
-declare interface BookType {
+interface BookType {
   title: string;
   id: string;
   column: ColumnType;
-};
+}
 
 declare type ColumnType = "toRead" | "reading" | "done";
+
+interface GeneralInfo {
+  bookName?: string | null;
+  issueDate?: string | null;
+  dueDate?: string | null;
+}
+
+interface BookDetails {
+  book?: string | null;
+  bookTitle?: string | null;
+  bookAuthor?: string | null;
+  bookGenre?: string | null;
+  bookLanguage?: string | null;
+  thumbnail?: string | null;
+  yearPublished?: string | null;
+  bookPublisher?: string | null;
+}
+
+interface AuthorDetails {
+  name?: string | null;
+  authorName?: string | null;
+  nationality?: string | null;
+  gender?: string | null;
+  penName?: string | null;
+  authorPhoto?: string | null;
+  website?: string | null;
+  activeYears?: string | null;
+}
+
+interface BookSections {
+  sections: Section[];
+}
+
+interface Section {
+  sectionTitle: string;
+  time?: number;
+  pages?: number;
+}
+
+interface Notes {
+  note?: string | null;
+  language?: string;
+}
+
+type BookData = GeneralInfo & 
+  BookDetails &
+  AuthorDetails &
+  BookSections &
+  Notes;
